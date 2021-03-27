241937425c96fc71f8788089b036caacd3c32194
Jose Hurtado <josehumberto17@gmail.com>
2021-03-27 15:53:17 -0500

    Add page object redmineproject

--- src/main/java/com/company/pages/RedmineProjectsPage.java
+++ src/main/java/com/company/pages/RedmineProjectsPage.java
@@ -1,7 +1,9 @@
 package com.company.pages;
 
+import com.company.models.RedmineProject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class RedmineProjectsPage extends RedmineLandingPage{
 
@@ -15,9 +17,36 @@ public class RedmineProjectsPage extends RedmineLandingPage{
     private By lblMessage = By.id("flash_notice");
 
 
-
-
     public RedmineProjectsPage(WebDriver driver) {
         super(driver);
     }
+
+    public void clickNewProject(){
+        driver.findElement(btnNewProject).click();
+    }
+
+    public void createProject(RedmineProject project){
+
+        driver.findElement(txtNameProject).sendKeys(project.getName());
+        driver.findElement(txtDescription).sendKeys(project.getDescription());
+        driver.findElement(txtHomePage).sendKeys(project.getHomePage());
+
+        WebElement checkboxPublic = driver.findElement(chbxPublicProject);
+
+        if(project.getPublic()){
+            if(!checkboxPublic.isSelected())
+                checkboxPublic.click();
+        }else{
+            if(checkboxPublic.isSelected())
+                checkboxPublic.click();
+        }
+
+        driver.findElement(btnCreate).click();
+    }
+
+    public String getUIMessage(){
+
+        return driver.findElement(lblMessage).getText();
+    }
+
 }
